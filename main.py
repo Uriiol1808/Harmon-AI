4cb92ead20fc76f374c933c89316c9d579549cfe
Uriiol1808 <oriolmarin18@gmail.com>
2024-02-03 17:00:58 +0100

    main.py update

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
+from pathlib import Path
 
 app = FastAPI()
+
+# Allow both localhost and 127.0.0.1 as origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost", "http://127.0.0.1"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Mount the Vue app as a static folder
 app.mount("/static", WSGIMiddleware(StaticFiles(directory="frontend/dist", html=True)))
