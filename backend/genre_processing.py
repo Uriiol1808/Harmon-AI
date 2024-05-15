6f2e701d9203c77877f8e1c7e49e03827a1587e5
Uri <oriolmarin18@gmail.com>
2024-05-15 15:26:51 +0200

    h5 weight file missing

--- backend/genre_processing.py
+++ backend/genre_processing.py
@@ -4,70 +4,66 @@ from keras.models import load_model
 
 class GenreProcessing:
     @staticmethod
-    def get_mfcc(path):
+    def get_mfcc(y, sr):
         """
         Mel Frequency Cepstral Coefficients (MFCC) describe the overall shape of
         the spectral envelope.
         """
-        y, sr = librosa.load(path, offset=0, duration=30)
         mfcc = librosa.feature.mfcc(y=y, sr=sr)
         return np.array(mfcc)
     
     @staticmethod
-    def get_mel_spectogram(path):
+    def get_mel_spectogram(y, sr):
         """
         Mel Spectogram is a spectorgam where frequencies are converted to the mel scale.
         """
-        y, sr = librosa.load(path, offset=0, duration=30)
         mel_spectogram = librosa.feature.melspectrogram(y=y, sr=sr)
         return np.array(mel_spectogram)
     
     @staticmethod
-    def get_chroma_vector(path):
+    def get_chroma_vector(y, sr):
         """
         Chroma-based features represent the tonal content of a musical audio signal
         in a condensed form.
         """
-        y, sr = librosa.load(path)
         chroma_vector = librosa.feature.chroma_stft(y=y, sr=sr)
         return np.array(chroma_vector)
 
     @staticmethod    
-    def get_tonnetz(path):
+    def get_tonnetz(y, sr):
         """
         Tonnetz is a pictorial representation of projected Chroma features onto a 6-
         dimensional basis representing the perfect fifth, minor third, and major third.
         """
-        y, sr = librosa.load(path)
         tonnetz = librosa.feature.tonnetz(y=y, sr=sr)
         return np.array(tonnetz)
     
     @staticmethod
-    def get_features(path):
+    def get_features(y, sr):
         """
         Concatenate all features into an array.
         """
 
         # MFCC feature
-        mfcc = GenreProcessing.get_mfcc(path)
+        mfcc = GenreProcessing.get_mfcc(y, sr)
         mfcc_feature = np.concatenate((mfcc.mean(axis=1),
                                        mfcc.min(axis=1),
                                        mfcc.max(axis=1)))
         
         # Mel Spectogram feature
-        mel_spectogram = GenreProcessing.get_mel_spectogram(path)
+        mel_spectogram = GenreProcessing.get_mel_spectogram(y, sr)
         mel_spectogram_feature = np.concatenate((mel_spectogram.mean(axis=1),
                                                  mel_spectogram.min(axis=1),
                                                  mel_spectogram.max(axis=1)))
         
         # Chroma Vector feature
-        chroma_vector = GenreProcessing.get_chroma_vector(path)
+        chroma_vector = GenreProcessing.get_chroma_vector(y, sr)
         chroma_vector_feature = np.concatenate((chroma_vector.mean(axis=1),
                                                 chroma_vector.min(axis=1),
                                                 chroma_vector.max(axis=1)))
         
         # Tonnetz feature
-        tonnetz = GenreProcessing.get_tonnetz(path)
+        tonnetz = GenreProcessing.get_tonnetz(y, sr)
         tonnetz_feature = np.concatenate((tonnetz.mean(axis=1),
                                           tonnetz.min(axis=1),
                                           tonnetz.max(axis=1)))
@@ -76,13 +72,23 @@ class GenreProcessing:
         return feature
     
     @staticmethod
-    def model_evaluation():
+    def predict_genre(features):
         """
         Evaluate the model by loading the weights.
         """
-        model = load_model("models/sentiment_model.h5", custom_objects={
-            'features': GenreProcessing.get_features()
-        })
+        # model = load_model("models/sentiment_model.h5", custom_objects={
+        #     'features': GenreProcessing.get_features()
+        # })
 
-        prediction = model.predict()
+        # prediction = model.predict(features)
+        
+        genres = ['blues', 'classical', 'country', 'disco', 'hiphop',
+                'jazz', 'metal', 'pop', 'reggae', 'rock']
+        
+        # Create final dict combining genres and predictions
+        prediction_dict = {genres[0]: 0.1, genres[1]: 0.1, genres[2]: 0.1,
+                           genres[3]: 0.1, genres[4]: 0.1, genres[5]: 0.1,
+                           genres[6]: 0.1, genres[7]: 0.1, genres[8]: 0.1,
+                           genres[9]: 0.1}
+        return prediction_dict 
     
